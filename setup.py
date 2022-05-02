e2abc6eb42a1d0545ab0d1847ab22584fa3ddb0b
ctrl-felix <62290842+ctrl-Felix@users.noreply.github.com>
2022-05-02 15:40:52 +0200

    create pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='cosmospy_protobuf',
-    version='0.0.1',
-    description='This package contains a compiled python version of all cosmos protobuf files with their dependencies.',
-    author='ctrl-Felix',
-    author_email='dev@ctrl-felix.de',
-    packages=find_packages(),
-    install_requires=["protobuf"],
-    include_package_data=True
-)
+setup()
