facef240d9abf41d5423250e623dedc14b6150b1
ctrl-felix <62290842+ctrl-Felix@users.noreply.github.com>
2022-06-24 01:01:22 +0200

    create examples folder and first example

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+# Examples
+Here you'll find examples on how to use python with protobuf and grpc and it's a great starting point to get started with grpc and cosmos in general.
+
+## Contributing
+If you are already using cosmospy_protobuf we would appreciate any contribution to the examples! This is not bound to a coin and can be an example for any coin.
---
+++ examples/query_bonded_tokens.py
@@ -0,0 +1,24 @@
+import cosmospy_protobuf.cosmos.staking.v1beta1.query_pb2 as query_pb2
+import cosmospy_protobuf.cosmos.staking.v1beta1.query_pb2_grpc as query_pb2_grpc
+import grpc
+
+host = "osmosis.strange.love"
+port = 9090
+
+
+class Staking:
+    def __init__(self, c: grpc.Channel = grpc.insecure_channel(f"{host}:{port}")):
+        self.stub = query_pb2_grpc.QueryStub(c)
+        self._assets = {}
+
+    @property
+    def bonded(self):
+        """Return bonded and unbonded funds"""
+        r = self.stub.Pool(query_pb2.QueryPoolRequest())
+        return {
+            'bonded': int(r.pool.bonded_tokens),
+            'unbonded': int(r.pool.not_bonded_tokens)
+        }
+
+
+print(Staking().bonded)
