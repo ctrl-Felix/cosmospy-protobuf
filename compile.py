73e8bb64fcbb845c0925080718214d7b6ab7ded0
Restyled.io <commits@restyled.io>
2023-07-08 10:44:04 +0000

    Restyled by autopep8

--- compile.py
+++ compile.py
@@ -6,13 +6,16 @@ import sys
 import logging
 
 parser = argparse.ArgumentParser(description='Aggregate all protobuf files')
-parser.add_argument('-p', '--package_name', type=str, default="cosmospy_protobuf", help="Name for the package to build. This will aggregate all files in the src/{package_name} folder")
+parser.add_argument('-p', '--package_name', type=str, default="cosmospy_protobuf",
+                    help="Name for the package to build. This will aggregate all files in the src/{package_name} folder")
 args = parser.parse_args()
 
 
 package_name = 'src/' + args.package_name
-logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s:%(message)s', level=logging.DEBUG)
 absolute_path = os.path.abspath(package_name)
+
 
 def run_protoc(filepath):
     if os.path.basename(filepath) == "query.proto" or os.path.basename(filepath) == "service.proto":
@@ -62,7 +65,8 @@ def walk_through_project_and_fix_imports(directory):
     for root, dirs, files in os.walk(directory):
         for filename in files:
             if filename.endswith('.proto'):
-                fix_proto_imports(os.path.abspath(os.path.join(root, filename)))
+                fix_proto_imports(os.path.abspath(
+                    os.path.join(root, filename)))
                 logging.info(f"Fixed imports for {filename}")
 
 
@@ -73,6 +77,7 @@ def remove_all_compiled_python_files(directory):
                 logging.info(f"Deleting {os.path.join(root, filename)}")
                 os.remove(os.path.join(root, filename))
 
+
 def rename_any_proto_imports(directory):
     for root, dirs, files in os.walk(directory):
         for filename in files:
@@ -82,13 +87,11 @@ def rename_any_proto_imports(directory):
             if 'import "google/protobuf/any.proto";\n' in lines:
                 with open(os.path.join(root, filename), 'w') as file:
                     for line in lines:
-                        file.write(re.sub(r'^import "google/protobuf/any.proto";\n', 'import "google/protobuf/cosmos_any.proto";\n', line))
+                        file.write(re.sub(r'^import "google/protobuf/any.proto";\n',
+                                   'import "google/protobuf/cosmos_any.proto";\n', line))
 
 
-
-
-
-#rename_any_proto_imports(package_name)
+# rename_any_proto_imports(package_name)
 remove_all_compiled_python_files(package_name)
 walk_through_project_and_compile_proto(package_name)
 walk_through_project_and_fix_imports(package_name)
