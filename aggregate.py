f03aee606aab9b65da08bfcdbd60d595a8227c6f
xloem <0xloem@gmail.com>
2023-08-06 05:19:39 -0400

    Simplify aggregation (#16)
    * simplify aggregate path handling
    
    as i try out more chains, i find some have protobuf paths that don't fit the old patterns
    this patch simplifies the protobuf path patterns to work with more chains, but requires a config change to use
    
    * upgraded cosmos.json
    
    official google protobufs are kept out of the tree because they are built with a different grpcio_tools compiler version which can clash

--- aggregate.py
+++ aggregate.py
@@ -79,17 +79,7 @@ for repo_url, repo_config in coin_config.items():
         proto_dir = os.path.join(repo_dir, proto_folder)
         proto_path_list = proto_folder.split('/')
         target = repo_config["target"] + "/" if "target" in repo_config else ""
-
-        # If the path consists of 2 or less consider the latest entry to be the according path
-        if len(proto_path_list) <= 2:
-            proto_path_in_repo = proto_path_list[-1]
-        else: # If the path has more than 2 items remove the first one and take the rest
-            proto_path_list.pop(0)
-            proto_path_in_repo = "/".join(proto_path_list)
-
-        # Fix compiling for Google protobuf
-        if proto_path_in_repo == 'proto/google':
-            proto_path_in_repo = proto_path_in_repo.split('/')[1]
+        proto_path_in_repo = proto_path_list[-1]
 
         try:
             shutil.copytree(proto_dir, root_abs_path + "/" + (target if target else proto_path_in_repo), dirs_exist_ok=True, ignore=include_patterns("*.proto"))
